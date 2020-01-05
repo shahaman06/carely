b6a9c8089f403aed365b6b8522e4eb9a05252f58
Aman Shah <shahaman06@gmail.com>
2020-01-05 15:55:37 +0530

    Required import statement added to Elderly.py

--- Elderly.py
+++ Elderly.py
@@ -1,8 +1,13 @@
+from BaseInfo import BaseInfo
+from ReviewInfo import ReviewInfo
+
 # format the sequence of variables
 class Elderly{
     
     def __init__(self, *args, **kwargs):
     
+        bi = BaseInfo()
+        review = ReviewInfo()
         strikes = 0 # less than 5 strikes
         health_cond = ""
         flagged = False

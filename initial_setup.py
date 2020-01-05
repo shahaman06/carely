1c437dfbe2d786b9bca19c160a5487e52a086a94
Aman Shah <shahaman06@gmail.com>
2020-01-06 00:20:03 +0530

    corrected dateutil installation

--- initial_setup.py
+++ initial_setup.py
@@ -2,7 +2,7 @@ import os
 def main():
     cmd = "GRANT ALL PRIVILEGES ON *.* TO 'carely_admin'@'localhost' IDENTIFIED BY 'carely_admin';"
     os.system("echo 'Installing Dependencies'")
-    os.system("pip3 install mysql-connector-python pandas dateutil phonenumbers pandas")
+    os.system("pip3 install mysql-connector-python pandas python-dateutil==1.4 phonenumbers pandas")
     os.system('sudo mysql -u root -e "'+cmd+'"')
     os.system('sudo mysql -u carely_admin -p < skeleton.sql')
 

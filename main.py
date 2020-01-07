d4bd2756f017e0515d219e109426140d671dafb6
Aman Shah <shahaman06@gmail.com>
2020-01-07 16:02:44 +0530

    CLI Fuctionality added to main.py and miscellaneous.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import argparse
 
-miscellaneous = "Main Python file to use execute commands :- New Registation of Carely User\
+desc = "Main Python file to use execute commands :- New Registation of Carely User\
                 ,Booking Appointments, and View Records. "
-parser = argparse.ArgumentParser(description=miscellaneous)
+parser = argparse.ArgumentParser(description=desc)
 parser.add_argument("-i","--id",action="store",help = "Input of User Id for further function use")
 # Here constant is stored in case no argument is passed
 parser.add_argument("-r","--registration",action="store_true",help="To register new Carely User.")
@@ -13,6 +13,6 @@ arg = parser.parse()
 if arg.registration:
     os.system("python Main/Registration.py")
 elif not arg.view:
-    os.system("python Main/View.py --view --id "+arg.id)
+    os.system("python Main/View.py --view --id '"+arg.id+"'")
 elif arg.booking:
     os.system("python Main/Booking.py")
--- miscellaneous.py
+++ miscellaneous.py
@@ -1,12 +1,19 @@
+from Utility.util import verify_user,invalid_user
 import argparse
+from os import system as cli
 
-miscellaneous = "Miscellaneous Python file to use execute commands :- New Registation of Carely User\
-                ,Booking Appointments, and View Records. "
+miscellaneous = "Miscellaneous Python file to Update Carely user records and confirm Booking status and Verify Carely Users "
 parser = argparse.ArgumentParser(description=miscellaneous)
-parser.add_argument("-i","--id",action="store",help = "Input of User Id for further function use")
-# Here constant is stored in case no argument is passed
-parser.add_argument("-v","--view",const=True,nargs="?",help="to view records of Booking, Caretaker,Elderly,etc.")
-parser.add_argument("-b","--booking",help="To Book Appointment")
-parser.add_argument("-r","--registration",help="To register new Carely User.")
-
+parser.add_argument("-u","--update",action="store_true",help="To Book Appointment")
+parser.add_argument("-v","--verification",action="store_true",help="To register new Carely User.")
 args = parser.parse()
+
+uid = input("Enter a Valid Carely Id")
+if uid[:2] not in ["el,ct"]:
+    invalid_user()
+db = "Caretaker" if uid[:2] else "Elderly"
+if verify_user(uid,db,"id"):
+    if arg.update:
+        cli("python Miscellaneous/Update.py -i '"+uid+"'")
+    if arg.verification:
+        cli("python Miscellaneous/Verification.py -i '"+uid+"'")

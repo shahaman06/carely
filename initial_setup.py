dd3f6086d361cef5ee2bd3aff7531c5b2b4424df
Aman Shah <shahaman06@gmail.com>
2020-01-07 14:33:44 +0530

    Modified initial_setup.py

--- initial_setup.py
+++ initial_setup.py
@@ -1,13 +1,27 @@
 import os
-def main():
+from argparse import ArgumentParser as ap
+def install_db_dep():
+    """
+        Install Dependencies and Setup Database
+    """
     cmd = "GRANT ALL PRIVILEGES ON *.* TO 'carely_admin'@'localhost' IDENTIFIED BY 'carely_admin';"
     os.system("echo 'Installing Dependencies'")
     os.system("pip3 install mysql-connector-python pandas python-dateutil phonenumbers pandas")
     os.system('sudo mysql -u root -e "'+cmd+'"')
-    os.system('sudo mysql -u carely_admin -p < skeleton.sql')
+    os.system('sudo mysql -u carely_admin -p < Data\skeleton.sql')
+
+def load_data():
+    #to load data from .csv to database using certain format
+    #import pandas as pd
+
+def main():
+    parser = ap(help="To ")
+    #parser.add("-i","--install",action="store_true",help = "Installing Dependencies and Setting Up Database")
+    parser.add("-l","--load",action="store_true",help = "Load Data from csv file to database (File Should be present in Data/CSV Database Directory). Note: Only Sample Format to be used for structure and file name. For Sample Format of csv file Check Sample CSV Folder" )
+    arg = parser.parse()
+    install_db_dep()
+    if arg.load:
+        load_data()
 
 if __name__ == "__main__":
     main()
-
-#to load data from .csv to database using certain format
-#import pandas as pd
